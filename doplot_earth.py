cfb91b16968962c24c9fd41d7b25467ad535e295
Andres Asensio Ramos <aasensio@iac.es>
2025-02-21 13:31:37 +0000

    Updated comments on doplot_earth

--- doplot_earth.py
+++ doplot_earth.py
@@ -8,7 +8,6 @@ import pandas as pd
 import time
 import scipy.sparse.linalg
 from tqdm import tqdm
-import datetime
 import cartopy.feature as cfeature
 from opensimplex import OpenSimplex
 
@@ -32,6 +31,8 @@ def weighted_avg_and_std(values, weights, axis=0):
 
 class Testing(object):
     def __init__(self, gpu=0, checkpoint_1d=None, checkpoint_2d=None, K1d=3, K2d=3, model_type='sparse'):
+        
+        # Check if CUDA is available and set the device accordingly
         self.cuda = torch.cuda.is_available()
         self.gpu = gpu
         self.device = torch.device(f"cuda:{self.gpu}" if self.cuda else "cpu")
@@ -43,24 +44,31 @@ class Testing(object):
         self.checkpoint_1d = checkpoint_1d
         self.checkpoint_2d = checkpoint_2d
                 
+        # Load the models
         if (self.model_type == 'denoise'):
+
+            # Load model 1D. First define the network and then load the weights
             self.model_1d = model.Network(K=self.K1d, L=32, device=self.device, model_class='conv1d').to(self.device)        
             print('N. total parameters 1D : {0}'.format(sum(p.numel() for p in self.model_1d.parameters() if p.requires_grad)))
             print("=> loading checkpoint '{}'".format(self.checkpoint_1d))
-            checkpoint = torch.load(self.checkpoint_1d, map_location=lambda storage, loc: storage)
+            checkpoint = torch.load(self.checkpoint_1d, map_location=lambda storage, loc: storage, weights_only=True)
             self.model_1d.load_state_dict(checkpoint['state_dict'])
             print("=> loaded checkpoint '{}'".format(self.checkpoint_1d))    
             print(f"rho : {checkpoint['state_dict']['rho']}")
 
+            # Load model 1D. First define the network and then load the weights
             self.model_2d = model.Network(K=self.K2d, L=32, NSIDE=16, device=self.device, model_class='conv2d').to(self.device)        
             print('N. total parameters 2D : {0}'.format(sum(p.numel() for p in self.model_2d.parameters() if p.requires_grad)))
             print("=> loading checkpoint '{}'".format(self.checkpoint_2d))
-            checkpoint = torch.load(self.checkpoint_2d, map_location=lambda storage, loc: storage)
+            checkpoint = torch.load(self.checkpoint_2d, map_location=lambda storage, loc: storage, weights_only=True)
             self.model_2d.load_state_dict(checkpoint['state_dict'])
             print("=> loaded checkpoint '{}'".format(self.checkpoint_2d))
             print(f"rho : {checkpoint['state_dict']['rho']}")
 
     def draw_earth_continents(self):
+        """
+        Draw the Earth's continents on the Mollweide projection.
+        """
         coast = []
 
         for geom in cfeature.COASTLINE.geometries():
@@ -85,6 +93,7 @@ class Testing(object):
         p_orbit = 365.256363 * 24.0
         phi_orb = np.pi
 
+        # Point of view
         self.pov = pov
 
         if (self.pov == 'faceon'):
@@ -101,6 +110,7 @@ class Testing(object):
 
         phi_rot = np.pi/2.0
 
+        # Define the Earth's surface in healpix pixels
         nside = 16
         npix = hp.nside2npix(nside)        
         polar_angle, azimuthal_angle = hp.pixelfunc.pix2ang(nside, np.arange(npix))
@@ -109,13 +119,15 @@ class Testing(object):
         y = np.sin(polar_angle) * np.sin(azimuthal_angle)
         z = np.cos(polar_angle)
         
-        # From NASA Earth Observations
+        # Read the model of the Earth surface albedo from NASA Earth Observations
         tmp = pd.read_csv('earth_720.csv')    
         earth = tmp.values[:, :]
 
         # earth[earth < 99999.0] = 1.0
+        # Remove bad pixels
         earth[earth == 99999.0] = 0.0
         
+        # Define latitude and longitude and interpolate the read model into the healpix grid
         lat = 90. - 0.5*np.arange(359)
         lon = -180 + 0.5*np.arange(720)
 
@@ -144,11 +156,11 @@ class Testing(object):
                                     nobs_per_epoch)
             times = np.concatenate([times, epoch_times])
 
-        measurement_std = 0.001
-
+        # Use exocartographer to compute the illumination map of the Earth at different times        
         truth = exocartographer.IlluminationMapPosterior(times, np.zeros_like(times),
                                         measurement_std, nside=nside, nside_illum=nside)
 
+        # Orginal parameters of the Earth
         true_params = {
             'log_orbital_period':np.log(p_orbit),
             'log_rotation_period':np.log(p_rotation),
@@ -159,32 +171,44 @@ class Testing(object):
         truth.fix_params(true_params)
         p = np.concatenate([np.zeros(truth.nparams), simulated_map])
 
+        # Get the illumination matrix that transforms the albedo map into the lightcurve
         Phi_np = truth.visibility_illumination_matrix(p)[None, :, :]
 
+        # Compute the Tikhonov regularization parameter
         PhiT_Phi = Phi_np[0, :, :].T @ Phi_np[0, :, :]
         largest_eval = scipy.sparse.linalg.eigsh(PhiT_Phi, k=1, which='LM', return_eigenvectors=False)
         rho = 0.4 / largest_eval
         rho = torch.tensor(rho.astype('float32')).to(self.device)
 
+        # Move the illumination matrix and its transpose to the device
         Phi = torch.tensor(Phi_np.astype('float32')).to(self.device)
         PhiT = torch.transpose(Phi, 1, 2)
 
+        # Generate the lightcurve. This essentially computes the product of Phi and the albedo map
         n = len(times)
         light = truth.lightcurve(p)
+
+        # Add noise to the lightcurve
         light += measurement_std * np.random.randn(n)
 
+        # Move the lightcurve to the device
         light = torch.tensor(light[None, :, None].astype('float32')).to(self.device)
 
+        # Initial estimation of the albedo map
         np.random.seed(123)
         self.x0 = torch.tensor(np.random.rand(1, 3072).astype('float32')).to(self.device)
         self.x0 = torch.zeros((1, 3072)).to(self.device)
                 
+        # Set the models to evaluation mode
         self.model_1d.eval()
         self.model_2d.eval()
         
+        # Without computing the gradients
         with torch.no_grad():
                         
             start = time.time()
+
+            # Evaluate the models
             out_surface_1d, _ = self.model_1d(light, self.x0, Phi, PhiT, rho)
             print(f'Elapsed time 1D : {time.time()-start}')
 
@@ -194,27 +218,16 @@ class Testing(object):
         
         out_surface_1d = out_surface_1d[-1].squeeze().cpu().numpy()        
         out_surface_2d = out_surface_2d[-1].squeeze().cpu().numpy()        
-        
-        
-        # import ipdb
-        # ipdb.set_trace()
-
+                
         return simulated_map, out_surface_1d, out_surface_2d
 
     def doplot_earth_single(self):
 
         pov = 'faceon'
-
-        f, ax = pl.subplots(nrows=1, ncols=3, figsize=(15,8))
-
-        space = 0.03
-        pos_up = ax[0].get_position().bounds
-        pos_down = ax[-1].get_position().bounds
-        delta = (pos_up[1] + pos_up[2] - pos_down[1] - space) / 5.0
-
-            
+                  
         simulated_map, out_surface_1d, out_surface_2d = self.evaluate_earth(measurement_std=0.001, pov=pov)
 
+        f, ax = pl.subplots(nrows=1, ncols=3, figsize=(15,8))
                        
         pl.axes(ax[0])
         hp.mollview(simulated_map, hold=True, title='Original', cmap=pl.cm.viridis, rot=0, flip='geo')
